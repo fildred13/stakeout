7b1147e3eda0cff525ee00b60e873eb2bd92ab6f
Sean Carty <fildred13@gmail.com>
2026-03-29 18:24:26 -0400

    refactor: change PlannedAction time windows from TimeSpan to DateTime

--- src/simulation/objectives/PlannedAction.cs
+++ src/simulation/objectives/PlannedAction.cs
@@ -7,8 +7,8 @@ public class PlannedAction
 {
     public IAction Action { get; init; }
     public int TargetAddressId { get; init; }
-    public TimeSpan TimeWindowStart { get; init; }
-    public TimeSpan TimeWindowEnd { get; init; }
+    public DateTime TimeWindowStart { get; init; }
+    public DateTime TimeWindowEnd { get; init; }
     public TimeSpan Duration { get; init; }
     public string DisplayText { get; init; }
     public Objective SourceObjective { get; init; }
